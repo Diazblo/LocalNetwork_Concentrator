4c34fc96c9923df033e614ff4d18eb2aa3a822d1
Rushikesh <rushikesh.palkar99@gmail.com>
2022-07-02 23:38:21 +0530

    first commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,104 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32wlxx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RTC_N_PREDIV_S 10
+#define RTC_PREDIV_S ((1<<RTC_N_PREDIV_S)-1)
+#define RTC_PREDIV_A ((1<<(15-RTC_N_PREDIV_S))-1)
+
+
+/* Private config -----------------------------------------------------------*/
+#define LPUART_BAUDRATE 9600
+
+// Default Region
+// referred from demo_regions.c
+// Replaced in at_init() -- RegRegion = DEMO_DEFAULT_REGION; from demo_at.c
+#define DEMO_DEFAULT_REGION 4
+#define DEMO_DEFAULT_SUBREGION 0
+
+// Verbose Level
+// Referred from sys_conf.h
+// Replaced with #ifndef
+#define VERBOSE_LEVEL     VLEVEL_H
+
+/* Private config end -----------------------------------------------------------*/
+
+#if 0
+#define USARTx_RX_Pin GPIO_PIN_3
+#define USARTx_RX_GPIO_Port GPIOA
+#define USARTx_TX_Pin GPIO_PIN_2
+#define USARTx_TX_GPIO_Port GPIOA
+#else
+// PINS for LoRa E5 cp2102 UART
+#define USARTx_RX_Pin GPIO_PIN_6
+#define USARTx_RX_GPIO_Port GPIOB
+#define USARTx_TX_Pin GPIO_PIN_7
+#define USARTx_TX_GPIO_Port GPIOB
+#endif
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
